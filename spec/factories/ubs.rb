da36e57df1521179792b1e602cd2e137db298ca2
Marcelo Toledo <marcelotoledo5000@gmail.com>
2019-12-28 18:33:48 -0300

    Update ubs.rb
    Upgrade Faker

--- spec/factories/ubs.rb
+++ spec/factories/ubs.rb
@@ -2,15 +2,15 @@
 
 FactoryBot.define do
   factory :ubs do
-    name { Faker::LordOfTheRings.location }
+    name { Faker::Movies::LordOfTheRings.location }
     address { Faker::Address.street_name }
     city { Faker::Address.city }
     phone { Faker::PhoneNumber.phone_number }
-    latitude { Faker::Number.decimal(2, 6) }
-    longitude { Faker::Number.decimal(2, 3) }
-    size { Faker::Number.between(1, 3) }
-    adaptation_for_seniors { Faker::Number.between(1, 3) }
-    medical_equipment { Faker::Number.between(1, 3) }
-    medicine { Faker::Number.between(1, 3) }
+    latitude { Faker::Number.decimal(l_digits: 2, r_digits: 6) }
+    longitude { Faker::Number.decimal(l_digits: 2, r_digits: 3) }
+    size { Faker::Number.between(from: 1, to: 3) }
+    adaptation_for_seniors { Faker::Number.between(from: 1, to: 3) }
+    medical_equipment { Faker::Number.between(from: 1, to: 3) }
+    medicine { Faker::Number.between(from: 1, to: 3) }
   end
 end
